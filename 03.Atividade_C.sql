e9e5c89f7acfb2080b4c55806d51055ee23524f2
Samambaia27 <emillygabrielly.dasilva27@gmail.com>
2024-06-25 17:48:48 -0300

    Add files via upload

---
+++ 03.Atividade_C.sql
@@ -0,0 +1,3 @@
+insert into clinte(nome, cpf, endereco, email, telefone, uf)
+values
+('João Da Silva', 99988877766, 'Rua da saudade1004,centro Fortaleza Ceará', 'joao.silva@gmail.com', '61999887766', 'CE')
